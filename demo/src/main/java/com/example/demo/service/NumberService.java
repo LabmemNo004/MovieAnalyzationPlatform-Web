978772140d1ad568cdd7f8597895b98e1529a061
Dark Souls <745184533@qq.com>
2021-01-07 23:57:33 +0800

    debug

--- demo/src/main/java/com/example/demo/service/NumberService.java
+++ demo/src/main/java/com/example/demo/service/NumberService.java
@@ -18,24 +18,29 @@ public class NumberService {
         if(CommentMsg!=0)
         {
             numberReposiroty.updateNumberById3(CommentMsg);
+            return;
         }
 
         if(ArtistID==0&MovieID==0)
         {
             numberReposiroty.updateNumberById(userID);
+            return;
         }
         if(userID==0&&MovieID==0)
         {
             numberReposiroty.updateNumberById2(ArtistID);
+            return;
         }
         if(userID==0&ArtistID==0)
         {
             numberReposiroty.updateNumberById1(MovieID);
+            return;
         }
     }
 
     public numbers getSimpleNumbers()
     {
+
         return numberReposiroty.getSimpleNumbers();
     }
 
